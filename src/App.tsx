56c2ff4c256bc3c4be57feb8faad8423b9fd97ec
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-09 08:48:54 -0300

    feat: Add event name input and display
    This commit introduces a state variable to store the event name and updates the heading to display the entered name.
    
    - Adds a `useState` hook to manage the event name.
    - Updates the `h1` tag to display the event name.
    - Implements an `onChange` handler for the input field to update the state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import "./App.css"
+import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+
   return (
     <div>
-      <h1>Evento</h1>
+      <h1>Evento {name}</h1>
 
       <form>
-        <input type="text" placeholder="Nome do evento" />
+        <input
+          type="text"
+          placeholder="Nome do evento"
+          onChange={(e) => setName(e.target.value)}
+        />
         <span className="error">Nome é obrigatório</span>
 
         <input type="date" placeholder="Nome do evento" lang="pt-BR" />
@@ -27,5 +34,5 @@ export default function App() {
         <button type="submit">Salvar</button>
       </form>
     </div>
-  )
+  );
 }
